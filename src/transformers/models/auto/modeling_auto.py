d91158d8e4628f5cb1b132c331d76a3d95932e32
Hosein <mr.hrezaei@gmail.com>
2025-08-29 11:32:33 +0100

    Remove T5LaEncoderModel

--- src/transformers/models/auto/modeling_auto.py
+++ src/transformers/models/auto/modeling_auto.py
@@ -1760,7 +1760,6 @@ MODEL_FOR_TEXT_ENCODING_MAPPING_NAMES = OrderedDict(
         ("squeezebert", "SqueezeBertModel"),
         ("t5", "T5EncoderModel"),
         ("t5gemma", "T5GemmaEncoderModel"),
-        ("t5la", "T5LaEncoderModel"),
         ("umt5", "UMT5EncoderModel"),
         ("xlm", "XLMModel"),
         ("xlm-roberta", "XLMRobertaModel"),
